ac8f22af4595f5046b83da4600d21fc69068f528
mihudarius99 <126120209+mihudarius99@users.noreply.github.com>
2023-02-28 20:13:41 +0200

    Add files via upload

---
+++ FirstTest/test1_google.cy.js
@@ -0,0 +1,14 @@
+describe('Site Google.com', () => {
+
+//Testul numarul 1
+it('functioneaza cu o cautare basic', () => {
+    cy.visit("https://google.com");
+    cy.get("#L2AGLb").click();
+    cy.get(".gLFyf").type("betano").type('{enter}');
+
+    cy.get("#result-stats").should('exist');
+})
+
+
+
+})
