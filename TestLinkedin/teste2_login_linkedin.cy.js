0b81e955fc45e62e05c69c7dbd0becb1f057d147
mihudarius99 <126120209+mihudarius99@users.noreply.github.com>
2023-02-28 20:15:02 +0200

    Add files via upload

---
+++ TestLinkedin/teste2_login_linkedin.cy.js
@@ -0,0 +1,19 @@
+
+
+describe('On Linkedin', () => {
+
+    it("I can login",() => {
+        cy.visit("https://www.linkedin.com");
+        cy.get('.nav__button-secondary').click();
+        cy.get('#username').type('YourEmail');
+        cy.get('#password').type('YourPassword');
+        cy.get('.btn__primary--large').click();
+        cy.get('#ember15').should('exist');
+        cy.get('[data-test="login-button"]').click("{enter}");
+
+    })
+
+
+
+
+})
